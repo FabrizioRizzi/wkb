ac36d51c419aa8a7fd2d4b33249023cd9c5a5cce
Fabrizio Rizzi <fabrizio@fabriziorizzi.it>
2022-05-20 10:38:15 +0200

    Added next auth with github provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
+import { SessionProvider } from "next-auth/react"
 import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/globals.css";
 
-const MyApp = ({ Component, pageProps }) => (
-  <ChakraProvider>
-    <Component {...pageProps} />
-  </ChakraProvider>
+const MyApp = ({ Component, pageProps: { session, ...pageProps } }) => (
+  <SessionProvider session={session}>
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  </SessionProvider>
 );
 
 export default MyApp;
